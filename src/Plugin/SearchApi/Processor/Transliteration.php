b61eda4643dca6aaca93b095d5a88b3d70ef6987
Pieter Frenssen <pieter@frenssen.be>
2014-06-14 13:59:20 +0200

    Properly inject the transliteration service.

--- src/Plugin/SearchApi/Processor/Transliteration.php
+++ src/Plugin/SearchApi/Processor/Transliteration.php
@@ -7,7 +7,9 @@
 
 namespace Drupal\search_api\Plugin\SearchApi\Processor;
 
+use Drupal\Component\Transliteration\TransliterationInterface;
 use Drupal\search_api\Processor\FieldsProcessorPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @SearchApiProcessor(
@@ -29,14 +31,34 @@ class Transliteration extends FieldsProcessorPluginBase {
   protected $langcode = NULL;
 
   /**
-   * {@inheritdoc}
+   * Constructs a Transliteration object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Component\Transliteration\TransliterationInterface $transliterator
+   *   The transliteration service.
+   * @param string $langcode
+   *   (optional) The language code of the language the content is in. Defaults
+   *   to 'en' if not provided.
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition) {
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, TransliterationInterface $transliterator, $langcode = 'en') {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
-    // @todo Replace this with proper dependency injection.
-    $this->langcode = \Drupal::languageManager()->getDefaultLanguage()->id;
-    $this->transliterator = \Drupal::service('transliteration');
+    $this->transliterator = $transliterator;
+    $this->langcode = $langcode;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $transliterator = \Drupal::service('transliteration');
+    $langcode = \Drupal::languageManager()->getDefaultLanguage()->id;
+    return new static($configuration, $plugin_id, $plugin_definition, $transliterator, $langcode);
   }
 
   /**
